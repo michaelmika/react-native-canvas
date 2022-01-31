d5cafe1bc1d2c51132a3131a82c9a033cb4f9999
Michael Axel Raring <raring@campus.tu-berlin.de>
2022-01-31 17:06:26 +0100

    Revert "Revert "AutoImport of FontAwesome 4.7 & Fonts""
    This reverts commit 275a60d5016fd046d2be37adbc99df37cdf11dea.

--- src/ImageData.js
+++ src/ImageData.js
@@ -1,23 +1,39 @@
-import Canvas from './Canvas';
-import {webviewConstructor} from './webview-binders';
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
 
-@webviewConstructor('ImageData')
-export default class ImageData {
-  constructor(canvas, array, width, height, noOnConstruction) {
-    if (!(canvas instanceof Canvas)) {
-      throw new Error('ImageData must be initialized with a Canvas instance');
-    }
-    this.canvas = canvas;
-    if (this.onConstruction && !noOnConstruction) {
-      this.onConstruction(array, width, height);
-    }
+var _Canvas = _interopRequireDefault(require("./Canvas"));
+
+var _webviewBinders = require("./webview-binders");
+
+var _dec, _class;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ImageData = (_dec = (0, _webviewBinders.webviewConstructor)('ImageData'), _dec(_class = function ImageData(canvas, array, width, height, noOnConstruction) {
+  var _this = this;
+
+  _classCallCheck(this, ImageData);
+
+  this.postMessage = function (message) {
+    return _this.canvas.postMessage(message);
+  };
+
+  this.addMessageListener = function (listener) {
+    return _this.canvas.addMessageListener(listener);
+  };
+
+  if (!(canvas instanceof _Canvas.default)) {
+    throw new Error('ImageData must be initialized with a Canvas instance');
   }
 
-  postMessage = message => {
-    return this.canvas.postMessage(message);
-  };
+  this.canvas = canvas;
 
-  addMessageListener = listener => {
-    return this.canvas.addMessageListener(listener);
-  };
-}
+  if (this.onConstruction && !noOnConstruction) {
+    this.onConstruction(array, width, height);
+  }
+}) || _class);
+exports.default = ImageData;
